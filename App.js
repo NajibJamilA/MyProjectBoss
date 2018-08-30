7579829526187fb9bce4edd2b5f8143f14e443c2
Najib <astaghfirulloh05@gmail.com>
2018-08-30 19:23:23 +0700

    first project

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View ,Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,6 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.android}>Nama : Najib Jamil Abdurrahman</Text>
         <Text style={styles.android}>Kelas : XI RPL 1</Text>
         <Text style={styles.android}>No.Abs : 25</Text>
+        <Image source={require('E:/reactnative/horai25/najib.jpg')}></Image>
       </View>
     );
   }
